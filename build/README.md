ec05534139caae4b30aec613af60d841b75589b5
Arielle R Munters <arielle@reivant.se>
2026-05-29 13:27:40 +0200

    refactor: add build vs update option to build, adapt to new configs, include miarka configs in repo

--- build/README.md
+++ build/README.md
@@ -1,14 +1,33 @@
 Build for packaging,
 the script must be run from the directory where the pipeline will be packaged (`miarka_package` in the example below).
 
+
 ```bash
 module load miniconda3
 git clone https://github.com/clinical-genomics-uppsala/poppy_uppsala.git
 mkdir miarka_package && cd miarka_package
 
-POPPY_GMS_VERSION="v1.0.0" POPPY_GMS_REPO="https://github.com/genomic-medicine-sweden/poppy.git" \
-TAG_OR_BRANCH="v0.4.1" CONFIG_VERSION="develop" PIPELINE_NAME="poppy_uppsala" PYTHON_VERSION="3.9" \
-PIPELINE_GITHUB_REPO="https://github.com/clinical-genomics-uppsala/poppy_uppsala.git" \
-CONFIG_GITHUB_REPO="https://github.com/clinical-genomics-uppsala/poppy_uppsala_config.git" \
-bash ../poppy_uppsala/build/build_conda.sh poppy_uppsala_config/config/miarka/references/references.hg38.md5sums.yaml
+POPPY_GMS_VERSION="v3.0.0" POPPY_GMS_REPO="https://github.com/genomic-medicine-sweden/poppy.git" \
+POPPY_UU_VERSION="v2.0.0" PIPELINE_GITHUB_REPO="https://github.com/clinical-genomics-uppsala/poppy_uppsala.git" \
+PIPELINE_NAME="poppy_uppsala" PYTHON_VERSION="3.9" \
+PATH_TO_apptainer_cache="path/to/.sif/on/remote" \
+APPT_CACHE_STATUS="build"
+
+bash build_conda.sh poppy_uppsala_config/config/miarka/references/references.GRCh38.md5sums.yaml poppy_uppsala_config/config/miarka/references/references.PoN.md5sums.yaml
+```
+!!! note
+If you run on the compute node:
+export SINGULARITY_CACHEDIR=/projects/wp4/nobackup/singularity_cache/{user}
+
+Copy the following files and folders to the cluster (eg Miarka):
+* poppy_uppsala_{version}.tar.gz
+* design_and_ref_files.tar.gz
+* apptainer_cache
+
+
+Next step: Extract the tar files and the env file inside the poppy_uppsala_{version} folder:
+```bash
+cd poppy_uppsala_{version}
+mkdir poppy_uppsala/venv/
+tar -xzvf env.tar.gz -C poppy_uppsala/venv/
 ```
--- build/build_conda.sh
+++ build/build_conda.sh
@@ -3,106 +3,113 @@ set -e
 
 eval "$(conda shell.bash hook)"
 
+# ---- repos -----
 PIPELINE_NAME="poppy_uppsala"
-PIPELINE_GITHUB_REPO="https://github.com/clinical-genomics-uppsala/poppy_uppsala.git"
-TAG_OR_BRANCH="v0.4.1"
+POPPY_UU_REPO="https://github.com/clinical-genomics-uppsala/poppy_uppsala.git"
+POPPY_UU_VERSION="v2.0.0"
 # poppy_uppsala is build on the top of poppy GMS
-POPPY_GMS_VERSION="v1.0.0"
+POPPY_GMS_VERSION="v3.0.0"
 POPPY_GMS_REPO="https://github.com/genomic-medicine-sweden/poppy.git"
 CONFIG_GITHUB_REPO="https://github.com/clinical-genomics-uppsala/poppy_uppsala_config.git"
 CONFIG_VERSION="develop"
-PYTHON_VERSION="3.9"
+PYTHON_VERSION="3.9"  # 3.11 funkar ocksa
 
 # Clone git of poppy_uppsala to configure conda environment
-git clone --branch ${TAG_OR_BRANCH} ${PIPELINE_GITHUB_REPO}
+git clone --branch ${POPPY_UU_VERSION} ${POPPY_UU_REPO}
 cd ${PIPELINE_NAME}
 
 # Create and activate conda environment in the current directory, then install pipeline requirements
-conda create --prefix ./${PIPELINE_NAME}_${TAG_OR_BRANCH}_env python=${PYTHON_VERSION} -y
-conda activate ./${PIPELINE_NAME}_${TAG_OR_BRANCH}_env
+conda create --prefix ./${PIPELINE_NAME}_${POPPY_UU_VERSION}_env python=${PYTHON_VERSION} -y
+conda activate ./${PIPELINE_NAME}_${POPPY_UU_VERSION}_env
 conda install -c conda-forge pip -y
 
-if [ -d ${PIPELINE_NAME}_${TAG_OR_BRANCH} ];
+if [ -d ${PIPELINE_NAME}_${POPPY_UU_VERSION} ];
 then
-    rm -fr ${PIPELINE_NAME}_${TAG_OR_BRANCH}
+    rm -fr ${PIPELINE_NAME}_${POPPY_UU_VERSION}
 fi
 
-# The directory ${PIPELINE_NAME}_${TAG_OR_BRANCH} is created for the files that are to be packaged and transferred
+# The directory ${PIPELINE_NAME}_${POPPY_UU_VERSION} is created for the files that are to be packaged and transferred
 # elsewhere:
 # - the pipeline code for poppy_uppsala as well as the "base" code from Poppy GMS
 # - the conda environment used to run the pipeline
 # - the snakemake-wrappers
 # - the hydra-genetics modules
-mkdir -p ${PIPELINE_NAME}_${TAG_OR_BRANCH}
+mkdir -p ${PIPELINE_NAME}_${POPPY_UU_VERSION}
 
-# Clone git of poppy_uppsala
-git clone --branch ${TAG_OR_BRANCH} ${PIPELINE_GITHUB_REPO} ${PIPELINE_NAME}_${TAG_OR_BRANCH}/${PIPELINE_NAME}
-./${PIPELINE_NAME}_${TAG_OR_BRANCH}_env/bin/pip3 install --no-cache-dir -I -r ${PIPELINE_NAME}_${TAG_OR_BRANCH}/${PIPELINE_NAME}/requirements.txt
-conda pack --prefix ./${PIPELINE_NAME}_${TAG_OR_BRANCH}_env -o ${PIPELINE_NAME}_${TAG_OR_BRANCH}/env.tar.gz
-
-# Clone snakemake-wrappers and hydra-genetics
-mkdir -p ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics
+# Clone git of poppy_uppsala and package the env
+git clone --branch ${POPPY_UU_VERSION} ${POPPY_UU_REPO} ${PIPELINE_NAME}_${POPPY_UU_VERSION}/${PIPELINE_NAME}
+./${PIPELINE_NAME}_${POPPY_UU_VERSION}_env/bin/pip3 install --no-cache-dir -I -r ${PIPELINE_NAME}_${POPPY_UU_VERSION}/${PIPELINE_NAME}/requirements.txt
+conda pack --prefix ./${PIPELINE_NAME}_${POPPY_UU_VERSION}_env -o ${PIPELINE_NAME}_${POPPY_UU_VERSION}/env.tar.gz
 
 # Clone the relevant branch only of Poppy GMS
-git clone --single-branch --branch ${POPPY_GMS_VERSION} ${POPPY_GMS_REPO} ${PIPELINE_NAME}_${TAG_OR_BRANCH}/poppy
+git clone --single-branch --branch ${POPPY_GMS_VERSION} ${POPPY_GMS_REPO} ${PIPELINE_NAME}_${POPPY_UU_VERSION}/poppy
+
 
 # Clone wrappers
-git clone https://github.com/snakemake/snakemake-wrappers.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/snakemake-wrappers
+git clone https://github.com/snakemake/snakemake-wrappers.git ${PIPELINE_NAME}_${POPPY_UU_VERSION}/snakemake-wrappers
 
 # Clone hydra modules
-git clone https://github.com/hydra-genetics/alignment.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/alignment
-git clone https://github.com/hydra-genetics/annotation.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/annotation
-git clone https://github.com/hydra-genetics/biomarker.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/biomarker
-git clone https://github.com/hydra-genetics/cnv_sv.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/cnv_sv
-git clone https://github.com/hydra-genetics/compression.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/compression
-git clone https://github.com/hydra-genetics/filtering.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/filtering
-git clone https://github.com/hydra-genetics/fusions.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/fusions
-git clone https://github.com/hydra-genetics/misc.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/misc
-git clone https://github.com/hydra-genetics/mitochondrial ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/mitochondrial
-git clone https://github.com/hydra-genetics/parabricks ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/parabricks
-git clone https://github.com/hydra-genetics/prealignment.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/prealignment
-git clone https://github.com/hydra-genetics/qc.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/qc
-git clone https://github.com/hydra-genetics/reports.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/reports
-git clone https://github.com/hydra-genetics/snv_indels.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/snv_indels
-git clone https://github.com/hydra-genetics/references.git ${PIPELINE_NAME}_${TAG_OR_BRANCH}/hydra-genetics/references
+mkdir -p ${PIPELINE_NAME}_${POPPY_UU_VERSION}/hydra-genetics
+git clone https://github.com/hydra-genetics/alignment.git ${PIPELINE_NAME}_${POPPY_UU_VERSION}/hydra-genetics/alignment
+git clone https://github.com/hydra-genetics/annotation.git ${PIPELINE_NAME}_${POPPY_UU_VERSION}/hydra-genetics/annotation
+git clone https://github.com/hydra-genetics/cnv_sv.git ${PIPELINE_NAME}_${POPPY_UU_VERSION}/hydra-genetics/cnv_sv
+git clone https://github.com/hydra-genetics/filtering.git ${PIPELINE_NAME}_${POPPY_UU_VERSION}/hydra-genetics/filtering
+git clone https://github.com/hydra-genetics/fusions.git ${PIPELINE_NAME}_${POPPY_UU_VERSION}/hydra-genetics/fusions
+git clone https://github.com/hydra-genetics/prealignment.git ${PIPELINE_NAME}_${POPPY_UU_VERSION}/hydra-genetics/prealignment
+git clone https://github.com/hydra-genetics/qc.git ${PIPELINE_NAME}_${POPPY_UU_VERSION}/hydra-genetics/qc
+git clone https://github.com/hydra-genetics/reports.git ${PIPELINE_NAME}_${POPPY_UU_VERSION}/hydra-genetics/reports
+git clone https://github.com/hydra-genetics/snv_indels.git ${PIPELINE_NAME}_${POPPY_UU_VERSION}/hydra-genetics/snv_indels
+git clone https://github.com/hydra-genetics/references.git ${PIPELINE_NAME}_${POPPY_UU_VERSION}/hydra-genetics/references # not used every time only when building pon
 
-## Keep DockerHub paths to create Singularity images
-mv ${PIPELINE_NAME}_${TAG_OR_BRANCH}/${PIPELINE_NAME}/config/config_uppsala_novaseq.yaml \
-${PIPELINE_NAME}_${TAG_OR_BRANCH}/${PIPELINE_NAME}/config/containers.yaml
+#  Replace POPPY_UU_VERSION with the chosen version of the pipeline for snakemake wrapper and hydragenetics paths
+sed -i -E "s/POPPY_UU_VERSION/${POPPY_UU_VERSION}/g" ./${PIPELINE_NAME}_${POPPY_UU_VERSION}/${PIPELINE_NAME}/profile/miarka/config.yaml
+sed -i -E "s/POPPY_UU_VERSION/${POPPY_UU_VERSION}/g" ./${PIPELINE_NAME}_${POPPY_UU_VERSION}/${PIPELINE_NAME}/config/config_offline.yaml # borde kanske vara separat med bara miarka paths?
 
-## Download the config files from the config repo
-git clone --branch ${CONFIG_VERSION} ${CONFIG_GITHUB_REPO} poppy_uppsala_config
-## copy resources.yaml files to the pipline config directory
-cp poppy_uppsala_config/config/miarka/*.yaml ./${PIPELINE_NAME}_${TAG_OR_BRANCH}/${PIPELINE_NAME}/config/
-cp -r poppy_uppsala_config/config/miarka/references ./${PIPELINE_NAME}_${TAG_OR_BRANCH}/${PIPELINE_NAME}/config/
-cp poppy_uppsala_config/profiles/miarka/config.yaml ./${PIPELINE_NAME}_${TAG_OR_BRANCH}/${PIPELINE_NAME}/profile/
-## replace TAG_OR_BRANCH in profiles by the chosen version of the pipeline
-sed -i -E "s/TAG_OR_BRANCH/${TAG_OR_BRANCH}/g" ./${PIPELINE_NAME}_${TAG_OR_BRANCH}/${PIPELINE_NAME}/profile/config.yaml
+# # Download containers and update container path in configs
+if [ ${APPT_CACHE_STATUS} == "build" ];
+then
+    hydra-genetics prepare-environment create-singularity-files -c ${PIPELINE_NAME}_${POPPY_UU_VERSION}/poppy/config/config_static.yaml -o apptainer_cache
+    cp ${PIPELINE_NAME}_${POPPY_UU_VERSION}/poppy/config/config_static.yaml ${PIPELINE_NAME}_${POPPY_UU_VERSION}/poppy/config/config_static.yaml.copy
+    hydra-genetics prepare-environment container-path-update -c ${PIPELINE_NAME}_${POPPY_UU_VERSION}/poppy/config/config_static.yaml.copy -n ${PIPELINE_NAME}_${POPPY_UU_VERSION}/poppy/config/config_static.yaml -p ${PATH_TO_apptainer_cache}
+    # config_marvin from poppy_uppsala also need to be updated
+    hydra-genetics prepare-environment create-singularity-files -c ${PIPELINE_NAME}_${POPPY_UU_VERSION}/${PIPELINE_NAME}/config/config_marvin.yaml -o apptainer_cache
+    cp ${PIPELINE_NAME}_${POPPY_UU_VERSION}/${PIPELINE_NAME}/config/config_marvin.yaml ${PIPELINE_NAME}_${POPPY_UU_VERSION}/${PIPELINE_NAME}/config/config_marvin.yaml.copy
+    hydra-genetics prepare-environment container-path-update -c ${PIPELINE_NAME}_${POPPY_UU_VERSION}/${PIPELINE_NAME}/config/config_marvin.yaml.copy -n ${PIPELINE_NAME}_${POPPY_UU_VERSION}/${PIPELINE_NAME}/config/config_marvin.yaml -p ${PATH_TO_apptainer_cache}
+fi
 
-# Pack all cloned repositories
-tar -zcvf ${PIPELINE_NAME}_${TAG_OR_BRANCH}.tar.gz ${PIPELINE_NAME}_${TAG_OR_BRANCH}
+# If apptainer already available remote and no new build is needed (specified by user). Only update path to cache in config.
+if [ ${APPT_CACHE_STATUS} == "update" ];
+then
+    cp ${PIPELINE_NAME}_${POPPY_UU_VERSION}/poppy/config/config_static.yaml ${PIPELINE_NAME}_${POPPY_UU_VERSION}/poppy/config/config_static.yaml.copy
+    hydra-genetics prepare-environment container-path-update -c ${PIPELINE_NAME}_${POPPY_UU_VERSION}/poppy/config/config_static.yaml.copy -n ${PIPELINE_NAME}_${POPPY_UU_VERSION}/poppy/config/config_static.yaml -p ${PATH_TO_apptainer_cache}
+    cp ${PIPELINE_NAME}_${POPPY_UU_VERSION}/${PIPELINE_NAME}/config/config_marvin.yaml ${PIPELINE_NAME}_${POPPY_UU_VERSION}/${PIPELINE_NAME}/config/config_marvin.yaml.copy
+    hydra-genetics prepare-environment container-path-update -c ${PIPELINE_NAME}_${POPPY_UU_VERSION}/${PIPELINE_NAME}/config/config_marvin.yaml.copy -n ${PIPELINE_NAME}_${POPPY_UU_VERSION}/${PIPELINE_NAME}/config/config_marvin.yaml -p ${PATH_TO_apptainer_cache}
+fi
 
-# Download containers
-conda activate ./${PIPELINE_NAME}_${TAG_OR_BRANCH}_env
-hydra-genetics prepare-environment create-singularity-files -c ./${PIPELINE_NAME}_${TAG_OR_BRANCH}/${PIPELINE_NAME}/config/containers.yaml -o apptainer_cache
+# # Pack all cloned repositories
+tar -zcvf ${PIPELINE_NAME}_${POPPY_UU_VERSION}.tar.gz ${PIPELINE_NAME}_${POPPY_UU_VERSION}
 
-# Download references
-for reference_config in "$@"
-do
-    hydra-genetics --debug references download -o design_and_ref_files -v $reference_config
-done
+
+# Download references if given on command line
+if [ "$#" != 0 ];
+then
+    for reference_config in "$@"
+    do
+        hydra-genetics --debug references download -o design_and_ref_files -v $reference_config
+    done
+    # Compress data
+    tar -czvf design_and_ref_files.tar.gz design_and_ref_files
+fi
+
 
 conda deactivate
 
-if [ -d ${PIPELINE_NAME}_${TAG_OR_BRANCH}_env ];
+if [ -d ${PIPELINE_NAME}_${POPPY_UU_VERSION}_env ];
 then
-    rm -fr ${PIPELINE_NAME}_${TAG_OR_BRANCH}_env
+    rm -fr ${PIPELINE_NAME}_${POPPY_UU_VERSION}_env
 fi
 
-if [ -d ${PIPELINE_NAME}_${TAG_OR_BRANCH} ];
+if [ -d ${PIPELINE_NAME}_${POPPY_UU_VERSION} ];
 then
-    rm -fr ${PIPELINE_NAME}_${TAG_OR_BRANCH}
+    rm -fr ${PIPELINE_NAME}_${POPPY_UU_VERSION}
 fi
-
-# Compress data
-tar -czvf design_and_ref_files.tar.gz design_and_ref_files
